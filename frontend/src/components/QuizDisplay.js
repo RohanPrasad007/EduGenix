24ca55e5d41bf79c1b647f6643665ff2633898c0
Riddhi-chavan <riddhic164@gmail.com>
2024-07-17 18:57:02 +0530

    added progress bar

--- frontend/src/components/QuizDisplay.js
+++ frontend/src/components/QuizDisplay.js
@@ -36,16 +36,16 @@ const QuizDisplay = ({ questions, onComplete }) => {
   };
 
   const question = questions[currentQuestion];
-
+  let position  = ((currentQuestion + 1) / questions.length) * 100
   return (
     <>
     <div className="min-h-screen rounded-3xl border-2 border-gray-300 ">
     <div className="w-full mx-auto">
       <div className="mx-8 mt-5  bg-gray-200 h-auto rounded-xl relative">
-        <div className="absolute -top-8 right-0 w-52">
+        <div className="absolute -top-8 right-0 w-52 hidden md:block">
           <img src="path.png" className="w-full "/>
         </div>
-        <div className="text-center font-bold  pt-3 text-2xl text-[#001454c0]">
+        <div className="text-xl text-center font-bold  pt-3 md:text-2xl text-[#001454c0]">
           Please Answer the Questions Carefully!
         </div>
         <div className="text-center  p-2 text-md text-[#001454c0]">
@@ -58,9 +58,19 @@ const QuizDisplay = ({ questions, onComplete }) => {
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -50 }}
       transition={{ duration: 0.5 }}
-      className="p-6 mt-8 mx-6 bg-white rounded-lg shadow-md"
+      className="p-6 mt-24 mx-6 bg-white rounded-lg shadow-md border-2 border-gray-300"
     >
-      <h2 className="text-xl font-bold mb-4 text-blue-800">
+      <div class='relative pt-4'>
+      <span className={`absolute left-${[position]}px bottom-0 mb-4 -translate-x-1/2 w-12 h-10 bg-white shadow-[0px_12px_30px_0px_rgba(16,24,40,0.1)] rounded-full px-3.5 py-2 text-gray-800 text-xs font-medium flex justify-center items-center after:absolute after:bg-white after:flex after:bottom-[-5px] after:left-1/2 after:-z-10 after:h-3 after:w-3 after:-translate-x-1/2 after:rotate-45`}>{currentQuestion + 1}</span>
+      <div className='relative flex w-full h-2.5  overflow-hidden rounded-3xl bg-gray-100'>
+      <div role='progressbar' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'  className='flex h-full items-center justify-center bg-indigo-600  text-white rounded-3xl' style={{
+            width: `${((currentQuestion + 1) / questions.length) * 100}%`,
+          }}> 
+        
+      </div>
+      </div>
+      </div>
+      <h2 className="text-xl font-bold mb-4 text-blue-800 pt-10">
         {question.question}
       </h2>
       <div className="space-y-2">
@@ -114,15 +124,8 @@ const QuizDisplay = ({ questions, onComplete }) => {
           <FaChevronRight className="ml-2" />
         </button>
       </div>
-      <div className="mt-4 bg-gray-200 rounded-full h-2.5">
-        <div
-          className="bg-blue-600 h-2.5 rounded-full"
-          style={{
-            width: `${((currentQuestion + 1) / questions.length) * 100}%`,
-          }}
-        ></div>
-      </div>
     </motion.div>
+    
     </div>
     </div>
     </>
